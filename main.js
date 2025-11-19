c0a5ba61bcd6a2355fcdfeed98b4a72b99a710a6
kappez <114881577+kappez@users.noreply.github.com>
2025-11-19 19:36:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ import * as texts from "./texts.js";
 function changeText(text) {
   if (text == "esport") {
     document.getElementById("aboutlefttext").innerHTML = `<p>${texts.esportText}</p>`;
+    document.getElementById("aboutleft").style.backgroundColor = "#ff70a630";
   } else if (text == "products") {
     document.getElementById("aboutlefttext").innerHTML = `<p>${texts.productsText}</p>`;
+    document.getElementById("aboutleft").style.backgroundColor = "#ff977030";
   } else if (text == "development"){
     document.getElementById("aboutlefttext").innerHTML = `<p>${texts.developmentText}</p>`;
+    document.getElementById("aboutleft").style.backgroundColor = "#ffd67030";
   } else {
     alert('No bueno, no value in string from button');
   }
